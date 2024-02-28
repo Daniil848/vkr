4d796c8df75b5b24f83d886ce8f1f516c948a710
Daniil848 <ladygindaniil848@gmail.com>
2024-02-28 23:17:42 +0300

    get articles by useEfect

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { useParams } from 'react-router-dom';
+import { getSingleArticle } from '../app/mainSlice';
 
 const ArticlePage = () => {
+  const state = useAppSelector((state) => state.slice);
+  const dispatch = useAppDispatch();
+
+  const articleId = useParams();
+
+  useEffect(() => {
+    const articleID = Number(articleId);
+
+    dispatch(getSingleArticle(articleID));
+  }, [dispatch, state.article]);
+
   return <></>;
 };
 
--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { getAllArticles } from '../app/mainSlice';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
 
 const ArticlesPage = () => {
+  const state = useAppSelector((state) => state.slice);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllArticles());
+  }, [dispatch, state.articles]);
+
   return <></>;
 };
 
