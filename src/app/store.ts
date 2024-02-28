1abab08c17a1294db48581c6a9449a7c5eee440f
Daniil848 <ladygindaniil848@gmail.com>
2024-02-28 20:12:05 +0300

    create slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import mainSlice from './mainSlice';
+
 export const store = configureStore({
   reducer: {
+    slice: mainSlice,
   },
 });
 
