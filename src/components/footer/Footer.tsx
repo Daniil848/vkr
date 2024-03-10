14b8717b41b23f088efe2613830aa4f8d08f0255
Daniil848 <ladygindaniil848@gmail.com>
2024-03-10 18:44:50 +0300

    footer layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <div className={styles.footer}>
+        <p className={styles.content}>
+          Разработал студент группы 19СН1с: Ладыгин Даниил Львович
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
