4d5ad0024bc5e6ec289b3291b6569a224d073d86
Daniil848 <ladygindaniil848@gmail.com>
2024-03-11 20:10:22 +0300

    types file

--- src/app/mainSlice.ts
+++ src/app/mainSlice.ts
@@ -1,46 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-
 import { db } from '../firebase';
 import { collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
-
-export interface Section {
-  id: string;
-  name: string;
-}
-export interface Article {
-  id: string;
-  sectionId: number;
-  title: string;
-  text: string;
-}
-export interface Test {
-  id: string;
-  articleId: number;
-  sectionId: number;
-  title: string;
-  questions: {
-    id: number;
-    question: string;
-    correctAnswer: number;
-    answers: {
-      id: number;
-      answer: string;
-    }[];
-  }[];
-}
-export interface State {
-  sections: Section[];
-  article: Article | null;
-  articles: Article[];
-  test: Test | null;
-  tests: Test[];
-  loading: boolean;
-  error: boolean;
-  articlesPage: boolean;
-  searchArticles: boolean;
-}
+import { Article, Section, State, Test } from './types';
 
 const initialState: State = {
   sections: [],
