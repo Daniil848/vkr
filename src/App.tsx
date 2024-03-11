33c7edd35d8d25629be4a11f80d554050c31d1ff
Daniil848 <ladygindaniil848@gmail.com>
2024-03-11 12:21:02 +0300

    change styles

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,17 @@ function App() {
     <div className="App">
       <Header />
       {state.loading && <Loader />}
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/articles" element={<ArticlesPage />} />
-        <Route path="/articles/article/:articleID" element={<ArticlePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route
+            path="/articles/article/:articleID"
+            element={<ArticlePage />}
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
