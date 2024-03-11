77b0cde1968e597fa2706ae92e23feb44865cbc2
Daniil848 <ladygindaniil848@gmail.com>
2024-03-11 20:00:20 +0300

    about page layout

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -4,8 +4,13 @@ import styles from './AboutPage.module.scss';
 const AboutPage = () => {
   return (
     <>
-      <div>
-        <p>About Page</p>
+      <div className={styles.wrapper}>
+        <div className={styles.logo} />
+        <p>
+          Тема дипломной работы: WEb-ресурс для проверки знаний сотрудников ЧС
+          (Пожарная часть)
+        </p>
+        <p>Разработал: студент группы 19СН1с - Ладыгин Даниил Львович</p>
       </div>
     </>
   );
